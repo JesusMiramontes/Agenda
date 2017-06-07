f28dbb6cb657e0c09638b174bec372327d63edac
Jesus Miramontes <jesus_miramontes@me.com>
2017-06-06 22:42:06 -0500

    Encabezado

--- src/agendadecontactos/AgendaDeContactos.java
+++ src/agendadecontactos/AgendaDeContactos.java
@@ -1,76 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * INSTITUTO NACIONAL DE MÉXICO
+ * INSTITUTO TECNOLÓGICO DE LA LAGUNA
+ * TÓPICOS AVANZADOS DE PROGRAMACIÓN
+ * ENE-AGO 2017
+ * PROFESOR: ING. LUIS FERNANDO GIL VAZQUEZ
+ * ALUMNO: JOSÉ DE JESÚS MIRAMONTES GÓMEZ
+ * MATRICULA: 15131349
  */
 package agendadecontactos;
 
-import static agendadecontactos.Connect.createConnection;
-import static agendadecontactos.Connect.selectRestaurants;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import static agendadecontactos.Connect.insertContacto;
-
-/**
- *
- * @author USER
- */
 public class AgendaDeContactos {
-
-    /**
-     * @param args the command line arguments
-     */
-    
-//    private static String dbURL = "jdbc:derby://localhost:1527/restaurant;create=true;user=root;password=root;";
-//    private static String tableName = "restaurants";
-//    // jdbc Connection
-//    private static Connection conn = null;
-//    private static Statement stmt = null;
     
     public static void main(String[] args) {
-//        SlashScreen m = new SlashScreen();
-//        m.setSize(820, 284);
-//        m.setLocationRelativeTo(null);
-//        m.setVisible(true);
-        
-        //createConnection();
-        //insertContacto(0, "Jesus", "87188", "jesus@correo.com");
-        //selectRestaurants();
-        GUI g = new GUI();
-        g.setVisible(true);
+        SlashScreen m = new SlashScreen();
+        m.setSize(820, 284);
+        m.setLocationRelativeTo(null);
+        m.setVisible(true);
+               
     }
-    
-//    public static void createConnection()
-//    {
-//        try
-//        {
-//            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
-//            //Get a connection
-//            conn = DriverManager.getConnection(dbURL); 
-//        }
-//        catch (Exception except)
-//        {
-//            except.printStackTrace();
-//        }
-//    }
-//    
-//    private static void insertRestaurants(int id, String restName, String cityName)
-//    {
-//        try
-//        {
-//            stmt = conn.createStatement();
-//            stmt.execute("insert into " + tableName + " values (" +
-//                    id + ",'" + restName + "','" + cityName +"')");
-//            stmt.close();
-//        }
-//        catch (SQLException sqlExcept)
-//        {
-//            sqlExcept.printStackTrace();
-//        }
-//    }
-
-    
+       
     
 }
